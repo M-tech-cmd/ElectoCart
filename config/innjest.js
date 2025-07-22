f699a8463585899c28a1372257845150f5a089bc
M-tech-cmd <127322142+M-tech-cmd@users.noreply.github.com>
2025-07-22 14:03:34 +0300

    Update

--- config/innjest.js
+++ config/innjest.js
@@ -7,7 +7,7 @@ export const inngest = new Inngest({ id: "electrocart-next" });
 
 // Inngest Function to save user data to a database
 export const syncUserCreation = inngest.createFunction(
-    { id: "sync-user-from-clerk" },
+    { id: "delete-user-with-clerk" },
     { event: "clerk/user.created" },
     async ({ event }) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data;
