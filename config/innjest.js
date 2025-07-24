ab46bfd51aac5104a9414ca36203dd969fe1539a
M-tech-cmd <127322142+M-tech-cmd@users.noreply.github.com>
2025-07-24 16:44:49 +0300

    Update

--- config/innjest.js
+++ config/innjest.js
@@ -1,11 +1,3 @@
-import { Inngest } from "inngest";
-import connectDB from "./db";
-// NEW CORRECTED LINE: Use the specific models alias
-import User from '@/models/User'; // <--- CHANGE THIS LINE TO USE THE ALIAS
-
-// Create a client to send and receive events
-export const inngest = new Inngest({ id: "electrocart-next" });
-
 // Inngest Function to save user data to a database
 export const syncUserCreation = inngest.createFunction(
     { id: "sync-user-from-clerk" },
@@ -16,7 +8,8 @@ export const syncUserCreation = inngest.createFunction(
             _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
-            image_url: image_url
+            // CHANGE THIS LINE: Use 'imageUrl' to match your schema
+            imageUrl: image_url // Corrected assignment
         };
         await connectDB();
         await User.create(userData);
@@ -33,20 +26,10 @@ export const syncUserUpdation = inngest.createFunction(
             _id: id,
             email: email_addresses[0].email_address,
             name: first_name + ' ' + last_name,
-            image_url: image_url
+            // CHANGE THIS LINE: Use 'imageUrl' to match your schema
+            imageUrl: image_url // Corrected assignment
         };
         await connectDB();
         await User.findOneAndUpdate({ _id: id }, userData);
-    }
-);
-
-// Inngest Function to delete user data from database
-export const syncUserDeletion = inngest.createFunction(
-    { id: "delete-user-with-clerk" },
-    { event: "clerk/user.deleted" },
-    async ({ event }) => {
-        const { id } = event.data;
-        await connectDB();
-        await User.findByIdAndDelete(id);
     }
 );
--- models/User.js
+++ models/User.js
@@ -1,35 +1,28 @@
-// Inngest Function to save user data to a database
-export const syncUserCreation = inngest.createFunction(
-    { id: "sync-user-from-clerk" },
-    { event: "clerk/user.created" },
-    async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data;
-        const userData = {
-            _id: id,
-            email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
-            // CHANGE THIS LINE: Use 'imageUrl' to match your schema
-            imageUrl: image_url // Corrected assignment
-        };
-        await connectDB();
-        await User.create(userData);
-    }
-);
+import mongoose from "mongoose";
 
-// Inngest Function to update user data in database
-export const syncUserUpdation = inngest.createFunction(
-    { id: "update-user-from-clerk" },
-    { event: "clerk/user.updated" },
-    async ({ event }) => {
-        const { id, first_name, last_name, email_addresses, image_url } = event.data;
-        const userData = {
-            _id: id,
-            email: email_addresses[0].email_address,
-            name: first_name + ' ' + last_name,
-            // CHANGE THIS LINE: Use 'imageUrl' to match your schema
-            imageUrl: image_url // Corrected assignment
-        };
-        await connectDB();
-        await User.findOneAndUpdate({ _id: id }, userData);
-    }
-);
+const userSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+    },
+    cartItems: {
+        type: Object,
+        default: {},
+    },
+}, { minimize: false });
+
+const User = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default User;
