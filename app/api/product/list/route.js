6a40e7d9c7d1caf95d1a7d95230e1321c9ff9329
M-tech-cmd <127322142+M-tech-cmd@users.noreply.github.com>
2025-07-30 22:49:15 +0300

    Update

--- app/api/product/list/route.js
+++ app/api/product/list/route.js
@@ -1,13 +1,15 @@
 import connectDB from "@/config/db";
 import Product from "@/models/Product";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
 export async function GET(request){
     try {
         await connectDB();
+        const {userId} = getAuth(request);
 
-        const products = await Product.find({});
+        const products = await Product.find({ userId });
         return NextResponse.json({ success: true, products });
 
     } catch (error) {
